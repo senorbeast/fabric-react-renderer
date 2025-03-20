5a54cf0afb4d8c0df92fcfc6007db7423bcccd64
senorbeast <44683244+senorbeast@users.noreply.github.com>
2025-03-21 00:41:21 +0530

    refactor: remove direct export of FabricReactRenderer from index

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,3 @@ export { useFabricCanvasEvent } from './hooks/useFabricEvent.js';
 export { FabImageWithFallback } from './components/async/FabImage.js';
 export { fab } from './core/fab.js';
 export type { FabProxy } from './fabric-jsx.js';
-import * as FabricReactRenderer from './fabric-jsx.js';
-export { FabricReactRenderer };
